f310eb8a1a5b4eaf748eb1256002b395296e4d1d
komallsingh <singhkomal11711@gmail.com>
2026-08-18 15:56:18 +0530

    implented with authenticity

--- src/github.ts
+++ src/github.ts
@@ -1,28 +1,31 @@
+import "dotenv/config";
+
 export async function getUserActivity(username: string) {
     const url = `https://api.github.com/users/${username}/events`;
+
+    const token = process.env.GITHUB_TOKEN;
 
     const response = await fetch(url, {
         headers: {
             "Accept": "application/vnd.github+json",
-            "User-Agent": "gitrack-cli"
+            "User-Agent": "gitrack-cli",
+            "Authorization": `Bearer ${token}`
         }
     });
 
     if (response.status === 403) {
-        throw new Error(
-            "GitHub API rate limit exceeded. Please try again later."
-        );
+        throw new Error("GitHub API rate limit exceeded.");
     }
 
     if (response.status === 404) {
-        throw new Error(
-            `GitHub user '${username}' was not found.`
-        );
+        throw new Error(`GitHub user '${username}' was not found.`);
     }
 
     if (!response.ok) {
+        const errorBody = await response.text();
+
         throw new Error(
-            `GitHub API returned ${response.status}`
+            `GitHub API returned ${response.status}: ${errorBody}`
         );
     }
 
